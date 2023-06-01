a437fe10ed4a024cc1e607d0f99b8e7bfda84c28
yanbowen <yanbowen199407@gmail.com>
2023-06-01 10:02:30 +0800

    [fix] error-msg

--- error-msg/src/lib.rs
+++ error-msg/src/lib.rs
@@ -1,9 +1,9 @@
 #[macro_export]
 macro_rules! error_msg {
 	($msg:literal) => {
-		anyhow!(format!("{}:{} error_msg: {}", file!(), line!(), $msg))
+		anyhow::anyhow!(format!("{}:{} error_msg: {}", file!(), line!(), $msg))
 	};
     ($fmt:expr, $($arg:tt)*) => {
-        anyhow!(format!("{}:{} error_msg: {}", file!(), line!(), format!($fmt, $($arg)*)))
+        anyhow::anyhow!(format!("{}:{} error_msg: {}", file!(), line!(), format!($fmt, $($arg)*)))
     };
 }
