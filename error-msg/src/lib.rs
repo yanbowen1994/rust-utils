808d406b0eab06b690c5b3e23553ccdac5b5518c
yanbowen <yanbowen199407@gmail.com>
2023-06-01 16:00:21 +0800

    [fix] 允许输出非literal形式的string

--- error-msg/src/lib.rs
+++ error-msg/src/lib.rs
@@ -3,6 +3,9 @@ macro_rules! error_msg {
 	($msg:literal) => {
 		anyhow::anyhow!(format!("{}:{} error_msg: {}", file!(), line!(), $msg))
 	};
+	($msg:expr) => {
+		anyhow::anyhow!(format!("{}:{} error_msg: {}", file!(), line!(), $msg))
+	};
     ($fmt:expr, $($arg:tt)*) => {
         anyhow::anyhow!(format!("{}:{} error_msg: {}", file!(), line!(), format!($fmt, $($arg)*)))
     };
